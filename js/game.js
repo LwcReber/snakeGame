118ae65de1662b6cd7d6144541350bfbb69f940f
lwcReber <1079670310@qq.com>
2017-12-12 00:00:29 +0800

    添加移动

--- js/game.js
+++ js/game.js
@@ -22,36 +22,80 @@ let SnakeModel = {
   numCols: 5,
   snake: [],
   init () {
+    // 第一个元素是尾部
     for (row = 0; row < this.numRows; row++) {
-        for (col = 0; col < this.numCols; col++) {
-          this.snake.push({x: row, y: col});
-      }
+        this.snake.push({x: row, y: 0});
     }
+  },
+  snakeMove (direction) {
+    console.log(direction);
+    let newHead = { x: this.snake[this.snake.length - 1].x + 1, y: this.snake[this.snake.length - 1].y };
+    switch (direction) {
+    case 'left':
+        newHead = { x: this.snake[this.snake.length - 1].x - 1, y: this.snake[this.snake.length - 1].y }
+        break;
+    case 'up':
+        newHead = { x: this.snake[this.snake.length - 1].x, y: this.snake[this.snake.length - 1].y - 1 }
+        break;
+    case 'right':
+        newHead = { x: this.snake[this.snake.length - 1].x + 1, y: this.snake[this.snake.length - 1].y }
+        break;
+    case 'down':
+        newHead = { x: this.snake[this.snake.length - 1].x, y: this.snake[this.snake.length - 1].y + 1}
+        break;
+    default:
+        return;
+        break;
+    }
+    console.log(newHead);
+    // 添加蛇头部
+    this.snake.push(newHead);
+    // 清除画布
+    SnakeControl.clearRect(this.snake[0]);
+    // 移除蛇尾部
+    this.snake.shift();
   }
 }
 
 // 控制器  C
 let SnakeControl = {
+  speed: 300,
+  curDirect: 'right', // 记录当前方向
+  init () {
+    SnakeModel.init();
+    this.move();
+  },
   // 上下左右按键移动蛇的位置
   handleInput (keyCode) {
       switch (keyCode) {
       case 'left':
-
+          this.curDirect = keyCode;
           break;
       case 'up':
-
+          this.curDirect = keyCode;
           break;
       case 'right':
-
+          this.curDirect = keyCode;
           break;
       case 'down':
-
+          this.curDirect = keyCode;
           break;
       default:
           break;
       }
-    },
-
+  },
+  getSnake () {
+    return SnakeModel.snake;
+  },
+  move () {
+    setInterval(() => {
+      SnakeModel.snakeMove(this.curDirect);
+      SnakeView.drawSnake();
+    }, this.speed);
+  },
+  clearRect (tail) {
+    ctx.clearRect(tail.x * SnakeView.width, tail.y * SnakeView.height, SnakeView.width, SnakeView.height);
+  }
 }
 
 // 视图 V 绘制
@@ -61,17 +105,17 @@ let SnakeView = {
   row: 0,
   col: 0,
   snakeColor: '#89EA65',
+  snakeArr: '',
   init () {
-
+    this.snakeArr = SnakeControl.getSnake();
+    this.drawSnake();
   },
   drawSnake() {
-    snake.forEach((index, item) => {
+    this.snakeArr.forEach((item, index) => {
       ctx.fillStyle = this.snakeColor;
-      ctx.fillRect(item ,this.width, this.height);
-    })
+      ctx.fillRect(item.x * this.width, item.y * this.height, this.width, this.height);
+    });
   },
-  move () {
-
-  }
 }
+SnakeControl.init();
 SnakeView.init();
